39f20b2fc7f94189e6f09c8d3f4b32e79496d47b
Miyunn <kavintheperera@gmail.com>
2022-08-09 02:22:45 +0530

    layout

---
+++ Model/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/add-data', methods=['GET'])
+def add_data_get():
+    return ""
+
+@app.route('/add-data', methods=['POST'])
+def add_data_post():
+    return ""
+
+@app.route('/practice', methods=['GET'])
+def practice_get():
+    return ""    
+
+@app.route('/practice', methods=['POST'])
+def practice_post():
+    return ""    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
