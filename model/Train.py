1f0e1b4bb7403d7f5545c7ec4c33a15b7d46b48a
Miyunn <kavintheperera@gmail.com>
2022-09-06 19:42:12 +0530

    20 character detection
    - val acc : 96%
    - Used model is in the notebook file

--- model/Train.py
+++ model/Train.py
@@ -21,17 +21,20 @@ import os
 
 path = "dataset/train/"
 path_test = "dataset/test/"
-files = os.listdir(path)[:6]
-files_test = os.listdir(path_test)[:6]
-print(files)
-print(files_test)
+files = os.listdir(path)[:20]
+files_test = os.listdir(path_test)[:20]
+print("Detected train classes : ",files)
+print("Detected test classes : ",files_test)
 
-classes={'1':0, '2':1, '3':2, '4':3, '5':4, '6':5}
+
+classes={'1':0, '2':1, '3':2, '4':3, '5':4, '6':5, '7':6, '8':7, '9':8, '10':9, '11':10, '12':11, '25':12, '38':13, '51':14,
+        '64':15, '77':16, '90':17, '93':18, '105':19}
 
 
 # In[3]:
 
 
+print("Creating train data list")
 img_train=[]
 lbl_train=[]
 
@@ -41,12 +44,13 @@ for cl in classes:
         img = cv2.imread(pth+"/"+img_name,0)
         img_train.append(img)
         lbl_train.append(classes[cl])
-print("Dataset created succesfully!")  
+print("Train data list imported")  
 
 
 # In[4]:
 
 
+print("Creating test data list")
 img_test=[]
 lbl_test=[]
 
@@ -62,7 +66,7 @@ print("Test Data imported")
 # In[5]:
 
 
-#Converting in to array
+print("Converting list to array type")
 img_train = np.array(img_train)
 lbl_train = np.array(lbl_train)
 print("Train data type")
@@ -71,6 +75,7 @@ print(type(lbl_train))
 
 img_test = np.array(img_test)
 lbl_test = np.array(lbl_test)
+
 print("Train data type")
 print(type(img_test))
 print(type(lbl_test))
@@ -79,13 +84,14 @@ print(type(lbl_test))
 # In[6]:
 
 
-plt.imshow(img_train[257], cmap='gray')
-print(lbl_train[257])
+#plt.imshow(img_train[257], cmap='gray')
+#print(lbl_train[257])
 
 
 # In[7]:
 
 
+print("Reshapping Train and Test data")
 img_train = img_train.reshape(img_train.shape[0], img_train.shape[1], img_train.shape[2], 1)
 img_test = img_test.reshape(img_test.shape[0], img_test.shape[1], img_test.shape[2], 1)
 
@@ -93,36 +99,24 @@ img_test = img_test.reshape(img_test.shape[0], img_test.shape[1], img_test.shape
 # In[8]:
 
 
+print("Train and test data shapes")
 print(img_train.shape)
 print(img_test.shape)
-
-
-# In[9]:
-
-
-lbl_train = to_categorical(lbl_train, 6)
-lbl_test = to_categorical(lbl_test, 6)
 
 
 # In[10]:
 
 
-print(lbl_train[200])
-plt.imshow(img_train[200], cmap='gray')
-
-
-# In[11]:
-
-
-print(np.max(img_train[0]))
-print(np.min(img_train[0]))
+print("Changing lables to categorical")
+lbl_train = to_categorical(lbl_train, 20)
+lbl_test = to_categorical(lbl_test, 20)
 
 
 # In[12]:
 
 
-img_train = img_train / 255
-img_test = img_test / 255
+print(lbl_train[2372])
+plt.imshow(img_train[2372], cmap='gray')
 
 
 # In[13]:
@@ -135,53 +129,72 @@ print(np.min(img_train[0]))
 # In[14]:
 
 
-print(type(img_train[0][0][0][0]))
-print(type(img_test[0][0][0][0]))
+img_train = img_train / 255
+img_test = img_test / 255
 
 
 # In[15]:
 
 
-img_train = img_train.astype('float32')
-img_test = img_test.astype('float32')
+print(np.max(img_train[0]))
+print(np.min(img_train[0]))
 
 
 # In[16]:
 
 
 print(type(img_train[0][0][0][0]))
+print(type(img_test[0][0][0][0]))
 
 
 # In[17]:
 
 
-model = Sequential()
-model.add(Conv2D(256, kernel_size=5, activation='relu'))
-model.add(MaxPool2D(pool_size=2))
-model.add(Dropout(0.3))
-model.add(Conv2D(512, kernel_size=5, activation='relu'))
-model.add(MaxPool2D(pool_size=2))
-model.add(Dropout(0.3))
-model.add(Conv2D(1024, kernel_size=5, activation='relu'))
-model.add(MaxPool2D(pool_size=2))
-model.add(Dropout(0.3))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dense(50, activation='relu'))
-model.add(Dense(6, activation='softmax'))
+print("Convering to type to float32")
+img_train = img_train.astype('float32')
+img_test = img_test.astype('float32')
 
 
 # In[18]:
+
+
+print(type(img_train[0][0][0][0]))
+
+
+# In[22]:
+
+
+model = Sequential()
+model = Sequential()
+
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(80,80,1)))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding = 'same'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding = 'valid'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Flatten())
+model.add(Dense(64,activation ="relu"))
+model.add(Dense(128,activation ="relu"))
+model.add(Dense(20,activation ="softmax"))
+
+print("Model loaded")
+
+print("Model loaded")
+
+
+# In[23]:
 
 
 model.compile(optimizer=tensorflow.keras.optimizers.Adadelta(learning_rate=0.1),
              loss=tensorflow.keras.losses.categorical_crossentropy, metrics=['accuracy'])
 
 
-# In[19]:
+# In[24]:
 
 
-model.fit(img_train, lbl_train, epochs=20, batch_size=1, validation_data=(img_test, lbl_test))
+print("Training started")
+model.fit(img_train, lbl_train, epochs=10, batch_size=1, validation_data=(img_test, lbl_test))
 
 
 # In[ ]:
@@ -200,7 +213,8 @@ print('Test Accuracy :',score[1])
 # In[ ]:
 
 
-model.save('test/model.h5')
+model.save('test/model_smaller.h5')
+print("Model saved")
 
 
 # In[ ]:
