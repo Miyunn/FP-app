8575a1f08c5039fdff0518fc6d7ce0aedae24dc0
Kavinthe Perera <kavintheperera@gmail.com>
2022-10-18 00:00:43 +0530

    404 page and routing

--- server.py
+++ server.py
@@ -114,6 +114,11 @@ def guess_post():
         print(e)
         return render_template('error.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
     #app.run(host = '0.0.0.0')
